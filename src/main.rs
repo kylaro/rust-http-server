41ee4df23c5de590927bbdcb5282d5b8e4a270d7
Kylar <kylar.osborne@gmail.com>
2023-08-17 18:05:59 +1000

    Working startercode

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
 fn main() {
     println!("Hello, world!");
     
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..];    
-    let string_borrow: &str = &string;
-    let string_literal = "1234";
-
-    dbg!(string_borrow);
-
-    dbg!(&string);
-    dbg!(string_slice);
-    
-    // let server = Server::new("127.0.0.1:8080");
-    // server.run();
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
 
 
@@ -28,7 +18,7 @@ impl Server{
     }
     
     fn run(self) {
-        
+        println!("Listening on {}", self.address)
     }
 
 }
