46c6a1203b1fa26d262ecd0dca3ed17c6ca5489b
Kylar <kylar.osborne@gmail.com>
2023-08-17 18:04:40 +1000

    Playing with string slices

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 fn main() {
     println!("Hello, world!");
-    let server = Server::new("127.0.0.1:8080");
-    server.run();
+    
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..];    
+    let string_borrow: &str = &string;
+    let string_literal = "1234";
+
+    dbg!(string_borrow);
+
+    dbg!(&string);
+    dbg!(string_slice);
+    
+    // let server = Server::new("127.0.0.1:8080");
+    // server.run();
 }
 
 
