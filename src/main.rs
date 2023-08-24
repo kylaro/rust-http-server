38ba53ffee956e22cf4f3f16bb3ddd1d9f0f496f
Kylar <kylar.osborne@gmail.com>
2023-08-24 11:25:45 +0800

    Add Resuest and methods enum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+
     
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
@@ -20,5 +26,34 @@ impl Server{
     fn run(self) {
         println!("Listening on {}", self.address)
     }
-
 }
+
+enum Method{
+    GET, 
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+/* Request
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
+
+/* Response
+HTTP/1.1 200 OK\r\n
+HEADERS \r\n
+BODY
+*/
